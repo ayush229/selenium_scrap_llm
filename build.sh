f78c143d6dff670d624ab5453e533dd4ddd515be
ayush229 <53784806+ayush229@users.noreply.github.com>
2025-06-14 23:02:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-
 apt-get update
-apt-get install -y chromium wget gnupg unzip curl
-
-# Make sure Chrome is executable for u-chromedriver
-ln -s /usr/bin/chromium /usr/bin/google-chrome
+apt-get install -y curl unzip
+python -m playwright install
